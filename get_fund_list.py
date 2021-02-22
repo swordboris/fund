e3e6edada64b6c2ce0ca16c20c4bd670869bfa4f
boris <boris_wang2009@qq.com>
2021-02-22 09:15:39 +0800

    update get_fund_list to add value to history table

--- get_fund_list.py
+++ get_fund_list.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from database.DBTable import DBTable
 from database.dbCon import get_sqlite_conn
 from database.FundInfoRecord import FundInfo
+from database.FundDailyRecord import FundDailyRec
 
 import requests
 import js2py
@@ -25,29 +26,35 @@ def get_funds_info(code2company,table,pageIdx,pages=200):
     total_pages = int(db['pages'])
     curpage = int(db['curpage'])
     datas = db['datas']
+    showday = db['showday']
     
-    #print("page download finished!", total_pages, curpage)
+    print("page download finished!", total_pages, curpage)
 
     for data in datas:
         fundInfo = FundInfo()
         fundInfo.code = data[0]
         fundInfo.name = data[1]
         
+        dailyTable = DBTable(conn,"FundDaily_{}".format(fundInfo.code),FundDailyRec)         
+       
+        fundDaily = FundDailyRec()
+        
         try:
-            fundInfo.net_per_unit = float(data[3])               
-     
-            fundInfo.net_acc = float(data[4])                
-    
-            fundInfo.daily_inc_val = float(data[7])
+            if len(data[3]):
+                fundInfo.net_per_unit = float(data[3])               
+            if len(data[4]):
+                fundInfo.net_acc = float(data[4])                
+            if len(data[7]):
+                fundInfo.daily_inc_val = float(data[7])
                 
-     
-            fundInfo.daily_inc_pct = float(data[8])
+            if len(data[8]): 
+                fundInfo.daily_inc_pct = float(data[8])
                 
             fundInfo.buy_ava = (data[9] == u'开放申购')
             fundInfo.sell_ava = (data[10] == u'开放赎回')
             
-    
-            fundInfo.fee = float(data[17][:-1])
+            if len(data[17]):
+                fundInfo.fee = float(data[17][:-1])
             
             fundInfo.company_code = code2company[fundInfo.code]
         except:
@@ -55,6 +62,15 @@ def get_funds_info(code2company,table,pageIdx,pages=200):
         
         fundInfo.update_date = datetime.now().date()
         table.addRecords([fundInfo])
+        
+        fundDaily.date = datetime.strptime(showday[0],"%Y-%m-%d").date()
+        fundDaily.dwjz = fundInfo.net_per_unit
+        fundDaily.ljjz = fundInfo.net_acc
+        fundDaily.zzl = fundInfo.daily_inc_pct
+        
+        dailyTable.addRecords([fundDaily])
+        
+        
         
     if curpage < total_pages:
         get_funds_info(code2company,table,pageIdx + 1,pages)
@@ -67,6 +83,7 @@ if __name__ == '__main__':
     with get_sqlite_conn("fund") as conn:
         ## create table FundInfo 
         fundInfoTable = DBTable(conn,"FundInfo",FundInfo)
+        
         
             ## get company code from table
         df = fundInfoTable.getDataFrame(None,['code','company_code'])
